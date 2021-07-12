9111c8676dd869495c8c52deb151f93008c8b927
bobby <bobbycai@hk.chinamobile.com>
2021-07-13 00:50:48 +0800

    add onclick function

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,6 +1,7 @@
 <template>
 	<view>
-		<cu-custom bgColor="bg-gradual-pink" :isBack="true"><block slot="backText">返回</block>
+		<cu-custom bgColor="bg-gradual-pink" :isBack="true">
+			<block slot="backText">返回</block>
 			<block slot="content">轮播图</block>
 		</cu-custom>
 		<view class="cu-bar bg-white">
@@ -11,11 +12,13 @@
 				<switch @change="DotStyle" :class="dotStyle?'checked':''" :checked="dotStyle?true:false"></switch>
 			</view>
 		</view>
-		<swiper class="screen-swiper" style="min-height:600rpx" :class="dotStyle?'square-dot':'round-dot'" :indicator-dots="true" :circular="true"
-		 :autoplay="true" interval="5000" duration="500">
-			<swiper-item v-for="(item,index) in swiperList" :key="index">
-				<image :src="item.url" mode="aspectFill" v-if="item.type=='image'"></image>
-				<video :src="item.url" autoplay loop muted :show-play-btn="false" :controls="false" objectFit="cover" v-if="item.type=='video'"></video>
+		<swiper class="screen-swiper" style="min-height:600rpx" :class="dotStyle?'square-dot':'round-dot'"
+			:indicator-dots="true" :circular="true" :autoplay="true" interval="5000" duration="500">
+			<swiper-item v-for="(item,index) in swiperList" :key="index" @click="toUrl(item.path)">
+				<image :src=" item.url" mode="aspectFit" v-if="item.type=='image'">
+				</image>
+				<video :src="item.url" autoplay loop muted :show-play-btn="false" :controls="false" objectFit="cover"
+					v-if="item.type=='video'"></video>
 			</swiper-item>
 		</swiper>
 	</view>
@@ -29,31 +32,38 @@
 				swiperList: [{
 					id: 0,
 					type: 'image',
-					url: '/static/pic/北京.png'
+					url: '/static/pic/北京.png',
+					path: '/pages/edu/edu'
 				}, {
 					id: 1,
 					type: 'image',
 					url: '/static/pic/上海.png',
+					path: '/pages/edu/edu'
 				}, {
 					id: 2,
 					type: 'image',
-					url: '/static/pic/深圳.png'
+					url: '/static/pic/深圳.png',
+					path: '/pages/edu/edu'
 				}, {
 					id: 3,
 					type: 'image',
-					url: '/static/pic/西安.png'
+					url: '/static/pic/西安.png',
+					path: '/pages/edu/edu'
 				}, {
 					id: 4,
 					type: 'image',
-					url: '/static/pic/广州.png'
+					url: '/static/pic/广州.png',
+					path: '/pages/edu/edu'
 				}, {
 					id: 5,
 					type: 'image',
-					url: '/static/pic/南京.png'
+					url: '/static/pic/南京.png',
+					path: '/pages/edu/edu'
 				}, {
 					id: 6,
 					type: 'image',
-					url: '/static/pic/南京.png'
+					url: '/static/pic/天津.png',
+					path: '/pages/edu/edu'
 				}],
 				dotStyle: false
 			};
@@ -65,8 +75,11 @@
 			// cardSwiper
 			cardSwiper(e) {
 				this.cardCur = e.detail.current
+			},
+			toUrl(url) {
+				this.$Common.navigateTo(url);
 			}
-			
+
 		}
 	}
 </script>
